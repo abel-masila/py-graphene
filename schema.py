66e51a40743d63f01a6be8c04f9739b80bd0439e
Abel <abelmasila@gmail.com>
2019-09-23 18:42:39 +0300

    added mutation to create user

--- schema.py
+++ schema.py
@@ -11,7 +11,7 @@ class User(ObjectType):
 
 
 class Query(ObjectType):
-    users = graphene.List(User, limit=graphene.Int() )
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
     is_admin = graphene.Boolean();
 
@@ -21,23 +21,42 @@ class Query(ObjectType):
     def resolve_is_admin(self, info):
         return True
 
-    def resolve_users(self, info,limit=None):
+    def resolve_users(self, info, limit=None):
         return [
-            User(id="1", username='masila', created_at=datetime.now()),
-            User(id="2", username='ben', created_at=datetime.now())
-        ][:limit]
+                   User(id="1", username='masila', created_at=datetime.now()),
+                   User(id="2", username='ben', created_at=datetime.now())
+               ][:limit]
 
 
-schema = graphene.Schema(query=Query)
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+
+    class Arguments:
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        print(username)
+        user = User(id="3", username=username, created_at=datetime.now())
+        return CreateUser(user=user)
+
+
+class Mutation(ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 rs = schema.execute('''
-  {
-        users  {
-            id
-            username
-            createdAt
-        }
-    }
+       mutation {
+        createUser(username:"Jeff") {
+            user {
+                id
+                username
+                createdAt
+            }
+            }
+       }
+    
     ''')
 dictResult = dict(rs.data.items())
 
