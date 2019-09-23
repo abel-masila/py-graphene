f62027ca88781049d887007ff928b005582b0a97
Abel <abelmasila@gmail.com>
2019-09-23 19:12:00 +0300

    added Post mutation

--- schema.py
+++ schema.py
@@ -5,6 +5,11 @@ from graphene import ObjectType
 from datetime import datetime
 
 
+class Post(ObjectType):
+    title = graphene.String()
+    content = graphene.String()
+
+
 class User(ObjectType):
     id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
@@ -14,7 +19,7 @@ class User(ObjectType):
 class Query(ObjectType):
     users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
-    is_admin = graphene.Boolean();
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return 'world'
@@ -41,22 +46,35 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user)
 
 
+class CreatePost(graphene.Mutation):
+    post = graphene.Field(Post)
+
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    def mutate(self, info, title, content):
+        post = Post(title=title, content=content)
+        return CreatePost(post=post)
+
+
 class Mutation(ObjectType):
     create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
 rs = schema.execute('''
-      query getusers ($limit: Int){
-        users(limit:$limit) {
-            id
-            username
-            createdAt
-      }
-      }
-    ''',
-                    variable_values={"limit": 1})
+      mutation {
+       createPost(title: "Hello", content: "World"){
+         post {
+            title
+            content 
+        }
+    }
+ }
+    ''')
 dictResult = dict(rs.data.items())
 
 print(json.dumps(dictResult, indent=3))
